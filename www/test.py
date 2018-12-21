532fee1c549ddbb42d5248bfcdce08b2e17f42da
superman <huangxigan@yeah.net>
2018-12-21 19:09:13 +0800

    实现test页面

--- www/test.py
+++ www/test.py
@@ -7,6 +7,7 @@ async def test(loop):
     u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
     await u.save()
 
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(test(loop))
